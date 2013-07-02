2abe326e72ec79d056efaa54aaaa780f5e6bce78
Phaneendra Chiruvella <hi@pcx.io>
2013-07-02 07:49:01 +0530

    adds setup.py, MANIFEST.in & updates .gitignore

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(name='Shoot',
+                 version='0.1dev',
+                 author='Phaneendra Chiruvella',
+                 author_email='hi@pcx.io',
+                 packages=['shoot',],
+                 url='https://github.com/pcx/shoot',
+                 description='Kick-start your prototype with Django templates',
+                 install_requires=["Django",],
+                 entry_points={
+                     'console_scripts': [
+                         ['shoot = shoot.run:run',],
+                     ]
+                 },
+                 package_data={'': ['LICENSE.txt', 'README.rst']},
+                 include_package_data=True,
+                 license=open('LICENSE').read(),
+                 long_description=open('README.rst').read(),
+             )
