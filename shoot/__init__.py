e7ed3c416a187ba7ab803c865359cdf71dbd255e
Phaneendra Chiruvella <hi@pcx.io>
2013-07-03 03:49:08 +0530

    shoot can, if present, use CWD/django_settings.py as Django settings module

--- shoot/__init__.py
+++ shoot/__init__.py
@@ -4,8 +4,16 @@ import sys
 from django.template import Context
 import django.template.loader
 
-sys.path.append(os.getcwd())
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shoot.django_settings")
+CWD = os.getcwd()
+sys.path.append(CWD)
+
+if os.path.exists(os.path.join(CWD, 'django_settings.py')):
+    django_settings_module = 'django_settings'
+    print("found custom django settings...")
+else:
+    django_settings_module = 'shoot.django_settings'
+    print('no custom django settings found, using defaults..')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", django_settings_module)
 
 config = {}
 
@@ -14,5 +22,5 @@ try:
     print("Custom context found...")
     config['context'] = Context(shootconfig.context)
 except ImportError:
-    print("No custom context found...")
+    print("No custom context found, using defaults...")
     config['context'] = Context()
