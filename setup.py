d115c429b6183f97c2e0eb991be88bd79d0e4256
Phaneendra Chiruvella <hi@pcx.io>
2013-07-03 03:54:21 +0530

    bumps version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 setuptools.setup(name='Shoot',
-                 version='0.0.1',
+                 version='0.1.0',
                  author='Phaneendra Chiruvella',
                  author_email='hi@pcx.io',
                  packages=['shoot',],
